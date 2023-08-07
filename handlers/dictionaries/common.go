c3bb4b9ad8af4fa276f9cfd29617a188c3da2939
rogeecn <rogeecn@qq.com>
2023-08-07 19:42:04 +0800

    format rpc errors

--- handlers/dictionaries/common.go
+++ handlers/dictionaries/common.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/atom-providers/jwt"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 type Common struct{}
@@ -12,12 +14,12 @@ type Common struct{}
 func (*Common) Claim(ctx context.Context, j *jwt.JWT) (*jwt.Claims, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return nil, jwt.TokenInvalid
+		return nil, status.Error(codes.Unauthenticated, "metadata is not provided")
 	}
 
 	tokens := md.Get(jwt.HttpHeader)
 	if len(tokens) == 0 {
-		return nil, jwt.TokenInvalid
+		return nil, status.Error(codes.Unauthenticated, "token is empty")
 	}
 
 	return j.ParseToken(tokens[0])
