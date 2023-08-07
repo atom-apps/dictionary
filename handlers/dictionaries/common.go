bd966261e86dc5d9b7874a78a6d25b1a0a16a5e6
rogeecn <rogeecn@qq.com>
2023-08-07 16:46:23 +0800

    use default metadata

--- handlers/dictionaries/common.go
+++ handlers/dictionaries/common.go
@@ -4,16 +4,21 @@ import (
 	"context"
 
 	"github.com/atom-providers/jwt"
-	"go-micro.dev/v4/metadata"
+	"google.golang.org/grpc/metadata"
 )
 
 type Common struct{}
 
 func (*Common) Claim(ctx context.Context, j *jwt.JWT) (*jwt.Claims, error) {
-	token, ok := metadata.Get(ctx, jwt.HttpHeader)
-	if ok {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
 		return nil, jwt.TokenInvalid
 	}
 
-	return j.ParseToken(token)
+	tokens := md.Get(jwt.HttpHeader)
+	if len(tokens) == 0 {
+		return nil, jwt.TokenInvalid
+	}
+
+	return j.ParseToken(tokens[0])
 }
